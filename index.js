a248cd33cb5b62f0c6050e35b6e03dfb4dbfefb0
Solomon Achilles <solohag92@gmail.com>
2022-01-24 21:07:23 +0000

    add ui & style.css

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const results = document.querySelector('.results');  
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
+let playerWins = 0;
+let computerWins = 0;
+
 
 function computerPlay(){
     const answers = ["Rock", "Paper", "Scissors"];
@@ -10,56 +16,61 @@ function getRandom(max) {
 }
 
 
+
 function playRound(playerSelection, computerSelection){
+    
     switch (computerSelection){
         case "Scissors":
             if (playerSelection.toUpperCase() === "ROCK"){
-                return "You win! Rock beats " + computerSelection;
+                results.textContent = "You win! Rock beats " + computerSelection;
                 break;
             } else if (playerSelection.toUpperCase() === "PAPER"){
-                return "You lose! " + computerSelection + " beats Paper";
+                results.textContent = "You lose! " + computerSelection + " beats Paper";
                 break;
             } else if (playerSelection.toUpperCase() === "SCISSORS"){
-                return "It\'s a tie";
+                results.textContent = "It\'s a tie";
                 break;
             }
        
         case "Paper":
             if (playerSelection.toUpperCase() === "SCISSORS"){
-                return "You win! Scissors beats " + computerSelection;
+                results.textContent = "You win! Scissors beats " + computerSelection;
                 break;
             } else if (playerSelection.toUpperCase() === "ROCK"){
-                return "You lose! " + computerSelection + " beats Rock";
+                results.textContent = "You lose! " + computerSelection + " beats Rock";
                 break;
             } else if (playerSelection.toUpperCase() === "PAPER"){
-                return "It\'s a tie"
+                results.textContent = "It\'s a tie"
                 break;
             }
         
         case "Rock":
             if (playerSelection.toUpperCase() === "PAPER"){
-                return "You win! Paper beats " + computerSelection;
+                results.textContent = "You win! Paper beats " + computerSelection;
                 break;
             } else if (playerSelection.toUpperCase() === "SCISSORS"){
-                return "You lose! " + computerSelection + " beats Scissors";
+                results.textContent = "You lose! " + computerSelection + " beats Scissors";
                 break;
             } else if (playerSelection.toUpperCase() === "ROCK"){
-                return "It\'s a tie"
+                results.textContent = "It\'s a tie"
                 break;
             }
         default :
             //
     }
-   
 }
 
-function game(){
-    const playerSelection = window.prompt("Make a choice between: Rock, Paper and Scissors", "Rock");
-    let playerWins = 0;
-    let computerWins = 0;
-    for (let i = 0; i < 5; i++){
-        let message = playRound(playerSelection,computerPlay());
-        console.log(i,  message)
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click', () => 
+    game(button.textContent)
+));
+
+function game(playerSelection){
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    let message = results.textContent;
         if (message.includes("win!")){
             playerWins += 1;
         } else if (message.includes("lose!")){
@@ -68,16 +79,22 @@ function game(){
             playerWins += 0;
             computerWins += 0;
         }
-    }
-   
-    if (playerWins > computerWins){
-        return "You win by " + playerWins + ": " + computerWins;
-    } else if (playerWins < computerWins){
-        return "You lose by " + computerWins + ": " + playerWins;
-    } else {
-        return "It\'s a tie, " + playerWins + ": " + computerWins;;
-    }
 
+    player.textContent = playerWins;
+    computer.textContent = computerWins;
+    if (playerWins >= 5 && computerWins <= 5){
+        results.textContent =  'Congrats! You won the game.'
+        buttons.forEach(button => button.disabled = true)
+    } else if (computerWins >= 5 && playerWins <= 5){
+        results.textContent =  'Sorry! You lost to computer'
+        buttons.forEach(button => button.disabled = true)
+    } else {
+        return;
+    }
 }
 
-console.log (game())
+
+
+
+
+
